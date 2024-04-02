699feccc43fe4fbc4c53e44bf6ca0c6f9869be89
Sarah Meftah <hichammeftah4@gmail.com>
2024-04-02 17:07:04 +0100

    migrating to my domain name

---
+++ js/events.js
@@ -0,0 +1,139 @@
+class Project {
+    constructor(title, description, teamsize, langcount, langsused, urlimage, url) {
+        this.title = title;
+        this.description = description;
+        this.teamsize = teamsize;
+        this.langcount = langcount;
+        this.langsused = langsused.split(',');
+        this.urlimage = urlimage;
+        this.url = url;
+    }
+}
+
+let Projects = []
+
+function setBgColor(color) {
+    style = getComputedStyle(document.body).getPropertyValue(color);
+    document.body.style.setProperty('--selectedColor', style);
+}
+
+function displayProject(project) {
+    let projectContainer = document.getElementById('Projects');
+    projectContainer.innerHTML += `
+    <div class="avatar fade-in-fwd" id="Project">
+        <div class="w-64 rounded">
+            <img src="${project.urlimage}" />
+        </div>
+    </div>`;
+}
+
+function fetchProjects() {
+    let projectContainer = document.getElementById('Projects');
+    LoadingAnimation.then(response => projectContainer.innerHTML = response);
+    fetch('http://192.168.1.11:8000/api/v1/projects' ,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.length === 0) {
+            projectContainer.innerHTML = '<h1 class="text-3xl text-center text-white">No projects available</h1>';
+            return;
+        }
+        projectContainer.innerHTML = '';
+        for ([key, value] of Object.entries(data)) {
+            let project = new Project(value.title, value.description, value.teamsize, value.langcount, value.langsused, value.image, value.url);
+            Projects.push(project);
+            setTimeout(displayProject(project), 2000 * key);
+        }
+    })
+    .catch(() => {
+        console.log('Error fetching projects');
+    });
+    
+}
+
+function loadAnimation() {
+    switch (window.location.pathname) {
+        case "/":
+            setBgColor('--magenta');
+            break;
+        case "/about":
+            setBgColor('--aqua');
+            break;
+        case "/projects":
+            setBgColor('--darkpurple');
+            break;
+        case "/experience":
+            setBgColor('--nightblue');
+            break;
+    }
+}
+
+function loadSideFunctions() {
+    switch (window.location.pathname) {
+        case "/":
+            break;
+        case "/about":
+            break;
+        case "/projects":
+            fetchProjects();
+            break;
+        case "/experience":
+            break;
+    }
+}
+
+function navigationListener() {
+    let Buttons = [];
+    Buttons.push(document.getElementById('AboutD'));
+    Buttons.push(document.getElementById('ProjectsD'));
+    Buttons.push(document.getElementById('ExperienceD'));
+    Buttons.push(document.getElementById('AboutM'));
+    Buttons.push(document.getElementById('ProjectsM'));
+    Buttons.push(document.getElementById('ExperienceM'));
+
+    Buttons.forEach((button) => {
+        button.addEventListener('mouseover', function() {
+            switch(button.id) {
+                case 'AboutD' :
+                case 'AboutM':
+                    button.classList.add('aqua-infinite');
+                    break;
+                case 'ProjectsD':
+                case 'ProjectsM':
+                    button.classList.add('darkpurple-infinite');
+                    break;
+                case 'ExperienceD':
+                case 'ExperienceM':
+                    button.classList.add('nightblue-infinite');
+                    break;
+            }
+        });
+        button.addEventListener('mouseout', function() {
+            switch(button.id) {
+                case 'AboutD' :
+                case 'AboutM':
+                    button.classList.remove('aqua-infinite');
+                    break;
+                case 'ProjectsD':
+                case 'ProjectsM':
+                    button.classList.remove('darkpurple-infinite');
+                    break;
+                case 'ExperienceD':
+                case 'ExperienceM':
+                    button.classList.remove('nightblue-infinite');
+                    break;
+            }
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.href.includes('index')) {
+        window.location.href = '/'
+    }
+    navigationListener();
+});
---
+++ js/router.js
@@ -0,0 +1,72 @@
+const LoadingAnimation = fetch('html/loading.html').then(response => response.text());
+const ErrorLoading = fetch('html/error.html').then(response => response.text());
+
+const routes = [
+    {
+        title: 'Hicham\'s Portfolio',
+        active: false,
+        path: '/',
+        content: () => GrabHTML('/html/home.html'),
+    },
+    {
+        title: 'About',
+        active: false,
+        path: '/about',
+        content: () => GrabHTML('/html/about.html'),
+    },
+    {
+        title: 'Projects',
+        active: false,
+        path: '/projects',
+        content: () => GrabHTML('/html/projects.html'),
+    },
+    {
+        title: 'Experience',
+        active: false,
+        path: '/experience',
+        content: () => GrabHTML('/html/experience.html'),
+    },
+]
+
+async function GrabHTML(path) {
+    const response = await fetch(path);
+    return await response.text();
+}
+
+async function router() {
+    const path = window.location.pathname;
+    const route = routes.find(route => route.path === path);
+    let mainContent = document.getElementById('mainSection');
+    if (route) {
+        if (route.active) return;
+        mainContent.innerHTML = await LoadingAnimation;
+        route.active = true;
+        document.title = route.title;
+        mainContent.innerHTML = await route.content();
+        for (let i = 0; i < routes.length; i++) {
+            if (routes[i].path !== path) {
+                routes[i].active = false;
+            }
+        }
+    } else {
+        mainContent.innerHTML = await ErrorLoading;
+    }
+    loadAnimation();
+    loadSideFunctions();
+}
+
+window.addEventListener('popstate', router);
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function (e) {
+            if (this.href.includes('smeftah.me')) {
+                e.preventDefault();
+                history.pushState(null, null, this.href);
+                router();
+            }
+        });
+    });
+
+});
+
+router();
