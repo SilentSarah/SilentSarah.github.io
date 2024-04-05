f1b3859c1fe8b24ab2320206bf669b11af67c900
Sarah Meftah <hichammeftah4@gmail.com>
2024-04-05 02:22:23 +0100

    Gigantic update, almost finished with project modals, implemented fetching mechanism for job experience, still WIP

--- js/events.js
+++ js/events.js
@@ -17,14 +17,57 @@ function setBgColor(color) {
     document.body.style.setProperty('--selectedColor', style);
 }
 
+function loadProjectModal(path) {
+    return (
+    fetch(path)
+    .then(response => response.text())
+    .then(data => {
+        let mainContainer = document.getElementById('mainSection');
+        if (mainContainer) {
+            mainContainer.innerHTML += data;
+        }
+    })
+    );
+}
+
+function setProjectInfo(project) {
+    let ProjectLogo = document.getElementById('ProjectLogo');
+    let ProjectTitle = document.getElementById('ProjectTitle');
+    let ProjectDescription = document.getElementById('ProjectDescription');
+    let ProjectTeamSize = document.getElementById('ProjectTeamSize');
+    let ProjectLangStack = document.getElementById('ProjectLangStack');
+
+    ProjectLogo.src = project.urlimage;
+    ProjectTitle.innerHTML = project.title;
+    ProjectDescription.innerHTML = project.description;
+    ProjectTeamSize.innerHTML = project.teamsize;
+    for (let lang of project.langsused) {
+        ProjectLangStack.innerHTML += `
+        <div class="tooltip" data-tip="${lang}">
+            <img class="w-16 drop-shadow-2xl" src="/img/Langs/${lang}.png">
+        </div>`;
+    }
+    document.getElementById("CloseBtn").addEventListener('click', () => {
+        document.getElementById('ProjectModal').remove();
+        projectInfoClickHandler();
+    });
+}
+
+function projectInfoClickHandler() {
+    document.querySelectorAll("#Project").forEach((project) => {
+        project.addEventListener('click', () => {
+            let index = Array.from(document.getElementById("Projects").children).indexOf(project);
+            loadProjectModal(`/html/projectInfo.html`).then(() =>
+                setProjectInfo(Projects[index])
+            );
+        });
+    });
+}
+
+
 function displayProject(project) {
     let projectContainer = document.getElementById('Projects');
-    projectContainer.innerHTML += `
-    <div class="avatar fade-in-fwd" id="Project">
-        <div class="w-64 rounded">
-            <img src="${project.urlimage}" />
-        </div>
-    </div>`;
+    projectContainer.innerHTML += `<img id="Project" class="rounded fade-in-fwd w-64" src="${project.urlimage}" />`;
 }
 
 function fetchProjects() {
@@ -46,13 +89,36 @@ function fetchProjects() {
         for ([key, value] of Object.entries(data)) {
             let project = new Project(value.title, value.description, value.teamsize, value.langcount, value.langsused, value.image, value.url);
             Projects.push(project);
-            setTimeout(displayProject(project), 2000 * key);
+            displayProject(project);
         }
+        projectInfoClickHandler();
     })
     .catch(() => {
         console.log('Error fetching projects');
     });
     
+}
+
+function fetchExperience() {
+    let experienceContainer = document.getElementById('Experiences');
+    LoadingAnimation.then(response => experienceContainer.innerHTML += response);
+    fetch('https://api.meftah.me/api/v1/experiences' ,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.length === 0 || Object.keys(data).length === 0) {
+            experienceContainer.innerHTML = '<h2 class="text-xl text-center text-white">No job experience available</h2>';
+            return;
+        }
+        experienceContainer.innerHTML = 'Found Something, This is a placeholder for now';
+    })
+    .catch(() => {
+        experienceContainer.innerHTML = '<h2 class="text-lg md:text-xl text-center text-white">Cannot connect to the backend server</h2>';
+    });
 }
 
 function loadAnimation() {
@@ -82,6 +148,7 @@ function loadSideFunctions() {
             fetchProjects();
             break;
         case "/experience":
+            fetchExperience();
             break;
     }
 }
